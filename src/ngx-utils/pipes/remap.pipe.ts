fd2b507bf44ada0b10cddaf674e7a4cfd58f81c2
stemy <balazs.stemler@metrix.co.hu>
2018-06-20 18:02:20 +0200

    Copy source object in remap pipe. v2.2.2

--- src/ngx-utils/pipes/remap.pipe.ts
+++ src/ngx-utils/pipes/remap.pipe.ts
@@ -11,7 +11,7 @@ export class RemapPipe implements PipeTransform {
     transform(source: any, map: any, defaultValue: string = "key"): any {
         if (!source) return empty;
         if (!map) return source;
-        const remapped = ObjectUtils.filter({map: source}, (value, key, target) => {
+        const remapped = ObjectUtils.filter({map: ObjectUtils.copy(source)}, (value, key, target) => {
             if (ObjectUtils.isArray(value) && value.every(ObjectUtils.isString)) {
                 target[key] = value.reduce((result, k) => {
                     result[k] = ObjectUtils.getValue(map, k, ObjectUtils.evaluate(defaultValue, {key: k, map: map}));
