bffedc1913defdbc5193239f05aba21e2bc21034
stemy <balazs.stemler@metrix.co.hu>
2018-08-30 18:08:37 +0200

    Safe html.-

--- src/ngx-utils/pipes/safe-html.pipe.ts
+++ src/ngx-utils/pipes/safe-html.pipe.ts
@@ -10,7 +10,20 @@ export class SafeHtmlPipe implements PipeTransform {
 
     }
 
-    transform(html: string): SafeHtml {
-        return this.sanitizer.bypassSecurityTrustHtml(html);
+    transform(value: string, type: string = "html"): SafeHtml {
+        switch (type) {
+            case "html":
+                return this.sanitizer.bypassSecurityTrustHtml(value);
+            case "style":
+                return this.sanitizer.bypassSecurityTrustStyle(value);
+            case "script":
+                return this.sanitizer.bypassSecurityTrustScript(value);
+            case "url":
+                return this.sanitizer.bypassSecurityTrustUrl(value);
+            case "resourceUrl":
+                return this.sanitizer.bypassSecurityTrustResourceUrl(value);
+            default:
+                throw new Error(`SafePipe unable to bypass security for invalid type: ${type}`);
+        }
     }
 }
