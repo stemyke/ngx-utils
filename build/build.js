d811c54125edd7e3141b59634fd8101992d091c3
stemler <balazs.stemler@metrix.co.hu>
2021-03-09 15:43:52 +0100

    Add build scripts.

---
+++ build/build.js
@@ -0,0 +1,30 @@
+const ngPackage = require('ng-packagr');
+const process = require('process');
+
+const buildType = process.argv[2];
+const buildCallbacks = [
+    {
+        type: 'ngx-utils',
+        cb: () => {
+            ngPackage
+                .ngPackagr()
+                .forProject('ng-package.json')
+                .build()
+                .catch(error => {
+                    console.error(error);
+                    process.exit(1);
+                });
+        }
+    }
+];
+
+if (!buildType) {
+    buildCallbacks.forEach(t => t.cb());
+} else {
+    const build = buildCallbacks.find(t => t.type === buildType);
+    if (!build) {
+        console.log('Nothing to build because of wrong build type: ', buildType);
+    } else {
+        build.cb();
+    }
+}
