fa77a9a5f7bd4daed8325a6deb43b3af4d94971d
stemy <balazs.stemler@metrix.co.hu>
2023-03-02 00:48:40 +0100

    Fix replace pipe.
    v12.2.10

--- src/ngx-utils/pipes/replace.pipe.ts
+++ src/ngx-utils/pipes/replace.pipe.ts
@@ -1,11 +1,11 @@
 import {Pipe, PipeTransform} from "@angular/core";
+import {ObjectUtils} from "../utils/object.utils";
 
 @Pipe({
     name: "replace"
 })
 export class ReplacePipe implements PipeTransform {
-
     transform(value: string, from: string | RegExp, to: string): string {
-        return value ? value.replace(from, to) : value;
+        return ObjectUtils.isDefined(value) ? `${value}`.replace(from, to) : ``;
     }
 }
