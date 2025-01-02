7195a84d7e833de38dd803fe112cf3bbd4357001
stemy <stemelr.balazs.akos@gmail.com>
2025-01-02 13:09:56 +0100

    Add some FileUtils.
    v19.1.3

--- src/ngx-utils/utils/file.utils.ts
+++ src/ngx-utils/utils/file.utils.ts
@@ -1,10 +1,12 @@
 import {HttpClient} from "@angular/common/http";
-import {first} from "rxjs/operators";
+import {firstValueFrom} from "rxjs";
 import {ObjectUtils} from "./object.utils";
 
 declare const saveAs: any;
 
 export class FileUtils {
+
+    static readonly base64: RegExp = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/g;
 
     static getExtension(file: File): string {
         return file ? file.name.substr(file.name.lastIndexOf(".")).toLowerCase() : null;
@@ -68,32 +70,41 @@ export class FileUtils {
         );
     }
 
-    static readDataFromUrl(http: HttpClient, url: string): Promise<string> {
-        return new Promise<string>(
-            // @dynamic
-            (resolve, reject) => {
-                if (!url) {
-                    reject({
-                        message: "The url is not specified"
-                    });
-                    return;
-                }
-                if ((/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/g).test(url)) {
-                    resolve(url);
-                    return;
-                }
-                first()(http.get(url, {
-                    responseType: "blob"
-                })).subscribe((blob: Blob) => {
-                    FileUtils.readFileAsDataURL(blob).then(resolve, reject);
-                }, reason => {
-                    if (reason.status > 0)
-                        reject(reason);
-                    else
-                        resolve(url);
-                });
-            }
-        );
+    static base64ToBlob(base64: string, mimeType = "application/octet-stream") {
+        // Decode the Base64 string into a binary string
+        const byteCharacters = atob(base64);
+
+        // Convert the binary string into an array of bytes
+        const byteNumbers = Array.from(byteCharacters, char => char.charCodeAt(0));
+        const byteArray = new Uint8Array(byteNumbers);
+
+        // Create a Blob with the byte array and the specified MIME type
+        return new Blob([byteArray], { type: mimeType });
+    }
+
+    static async readBlobFromUrl(http: HttpClient, url: string): Promise<Blob> {
+        if (!url) {
+            throw new Error(`The URL is not specified for readBlobFromUrl`);
+        }
+        if (url.match(FileUtils.base64)) {
+            return FileUtils.base64ToBlob(url);
+        }
+        return firstValueFrom(http.get(url, {
+            responseType: "blob"
+        }));
+    }
+
+    static async readDataFromUrl(http: HttpClient, url: string): Promise<string> {
+        if (!url) {
+            throw new Error(`The URL is not specified for readBlobFromUrl`);
+        }
+        if (url.match(FileUtils.base64)) {
+            return url;
+        }
+        const blob = await firstValueFrom(http.get(url, {
+            responseType: "blob"
+        }));
+        return FileUtils.readFileAsDataURL(blob);
     }
 
     private static readFile(callback: (reader: FileReader) => void): Promise<string> {
