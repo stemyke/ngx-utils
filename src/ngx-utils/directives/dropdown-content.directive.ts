8db349b94c5f442543edda8c7b005be76fa5b4ae
stemy <stemler.balazs.akos@gmail.com>
2025-02-14 11:08:30 +0100

    Fix injection dropdown content to parent element.
    v19.3.1

--- src/ngx-utils/directives/dropdown-content.directive.ts
+++ src/ngx-utils/directives/dropdown-content.directive.ts
@@ -1,6 +1,8 @@
-import {Directive, Input, OnDestroy, OnInit, Optional, TemplateRef, ViewContainerRef} from "@angular/core";
+import {Directive, Inject, OnDestroy, OnInit, Optional, TemplateRef, ViewContainerRef} from "@angular/core";
 import {Subscription} from "rxjs";
-import {autoPlacement, autoUpdate, computePosition, Middleware} from "@floating-ui/dom";
+import {autoPlacement, autoUpdate, computePosition, Middleware, Strategy} from "@floating-ui/dom";
+
+import {ROOT_ELEMENT} from "../common-types";
 import {DropdownDirective} from "./dropdown.directive";
 import {ObservableUtils} from "../utils/observable.utils";
 import {getCssVariables} from "../utils/misc";
@@ -14,12 +16,12 @@ const rectProps = ["x", "y", "width", "height"];
 })
 export class DropdownContentDirective implements OnInit, OnDestroy {
 
-    protected root: HTMLElement;
     protected subscription: Subscription;
     protected lastPlacement: string;
     protected cleanUp: () => void;
 
     constructor(protected vcr: ViewContainerRef,
+                @Optional() @Inject(ROOT_ELEMENT) protected rootElem: HTMLElement,
                 @Optional() protected dropdown: DropdownDirective,
                 @Optional() readonly templateRef: TemplateRef<any>) {
         if (!this.dropdown) {
@@ -45,19 +47,12 @@ export class DropdownContentDirective implements OnInit, OnDestroy {
     }
 
     protected createView(init: boolean = false) {
-        if (!this.root) {
-            const rootNode = this.vcr.element.nativeElement.getRootNode();
-            if (rootNode === document) {
-                this.root = document.body;
-            } else {
-                this.root = rootNode;
-            }
-        }
         const ref = this.dropdown.nativeElement;
         const content = this.createWrapper();
         this.dropdown.contentElement = content;
         // Set up floating UI positioning settings
         const placement = this.dropdown.placement || "bottom";
+        const strategy: Strategy = this.dropdown.attachToRoot && this.rootElem ? "fixed" : "absolute";
         const middleware: Middleware[] = [];
         if (this.dropdown.autoPlacement) {
             middleware.push(autoPlacement(this.dropdown.autoPlacement));
@@ -71,16 +66,17 @@ export class DropdownContentDirective implements OnInit, OnDestroy {
                     ref,
                     content,
                     {
-                        strategy: "fixed",
+                        strategy,
                         placement,
                         middleware
                     }
                 ).then(({x, y, placement}) => {
                     Object.assign(content.style, {
                         opacity: init ? "0" : "1",
-                        position: "fixed",
+                        position: strategy,
                         left: `${x}px`,
                         top: `${y}px`,
+                        zIndex: 1,
                     });
                     const refRect = ref.getBoundingClientRect();
                     const contentRect = content.getBoundingClientRect();
@@ -114,8 +110,8 @@ export class DropdownContentDirective implements OnInit, OnDestroy {
         const ref = this.vcr.createEmbeddedView(this.templateRef);
         ref.rootNodes.forEach(node => wrapper.appendChild(node));
 
-        if (this.dropdown.attachToRoot) {
-            this.root.appendChild(wrapper);
+        if (this.dropdown.attachToRoot && this.rootElem) {
+            this.rootElem.appendChild(wrapper);
             const referenceStyles = getCssVariables(this.dropdown.nativeElement);
             const wrapperStyles = getCssVariables(wrapper);
             Object.keys(referenceStyles).forEach(key => {
@@ -124,11 +120,24 @@ export class DropdownContentDirective implements OnInit, OnDestroy {
                 }
             });
         } else {
-            this.vcr.element.nativeElement?.appendChild(wrapper);
+            const anchor = this.vcr.element.nativeElement as HTMLElement;
+            anchor?.parentElement?.appendChild(wrapper);
         }
+        const autoPlacement = this.dropdown.autoPlacement;
         if (this.lastPlacement) {
             wrapper.classList.add(this.lastPlacement);
         }
+        if (autoPlacement) {
+            const vertical = autoPlacement.allowedPlacements.some(p => p.includes("top") || p.includes("bottom"));
+            const horizontal = autoPlacement.allowedPlacements.some(p => p.includes("left") || p.includes("right"));
+            if (vertical && horizontal) {
+                wrapper.classList.add(`dropdown-content-axis-both`);
+            } else {
+                const axis = vertical ? "vertical" : "horizontal";
+                wrapper.classList.add(`dropdown-content-axis-${axis}`);
+            }
+        }
+        wrapper.classList.add("dropdown-content-wrap");
         return wrapper;
     }
 
