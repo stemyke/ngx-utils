5d715a21deaadc015268cb5b3515b35873035435
stemy <balazs.stemler@metrix.co.hu>
2026-05-21 09:37:53 +0200

    Dependency cleanup.
    v19.9.33

--- watch.mjs
+++ watch.mjs
@@ -1,67 +1,55 @@
-import {spawn} from 'child_process';
 import {join, resolve} from 'path';
 import {deleteSync} from 'del';
+import cpy from 'cpy';
 import {existsSync, renameSync} from 'fs';
 import {program} from 'commander';
-import watch from 'node-watch';
-import {copy} from './build/copy.mjs';
+import {ngPackagr} from 'ng-packagr';
 
+const moduleNamespace = '@stemy';
+const moduleName = 'ngx-utils';
 program
-    .version('17.0.0', '-v, --version')
-    .option('-b, --skip-build', 'Skip first build')
-    .option('-m, --modules', 'Also copy node modules to project')
-    .option('-p, --project [path]', 'Project path where "ngx-utils" is used')
+    .version('19.9.0', '-v, --version')
+    .option('-p, --project [path]', `Project path where '${moduleName}' is used`)
     .parse(process.argv);
 
 const options = program.opts();
 const projectPath = typeof options.project !== 'string' ? null : resolve(options.project);
 const noProject = !projectPath;
 
-let child = null;
+/**
+ * @type {AbortController}
+ */
 let deploy = null;
 
 function deployToProject() {
-    if (noProject) return;
-    const metrixPath = join(projectPath, 'node_modules', '@stemy');
-    const distPath = join(metrixPath, 'dist');
-    deploy = copy('./dist/**', distPath, 'dist folder to project');
-    return deploy.then(() => {
-        const targetPath = join(metrixPath, 'ngx-utils');
-        if (existsSync(targetPath)) {
-            deleteSync(targetPath, {force: true});
-        }
-        renameSync(distPath, targetPath);
-    });
-}
-
-function build(cb = new Function("void 0")) {
-    if (child) {
-        child.kill();
-    }
     if (deploy) {
-        deploy.cancel();
+        deploy.abort();
     }
-    if (options.skipBuild) {
-        cb();
-        return;
-    }
-    console.log('Build started...');
-    child = spawn('node', ['build/build.mjs']);
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-    child.on('exit', () => {
-        console.log('Build ended');
-        deployToProject().then(() => {
-            if (typeof cb !== 'function') return;
-            cb();
+    if (noProject) return;
+    deploy = new AbortController();
+    const nsPath = join(projectPath, 'node_modules', moduleNamespace);
+    const distPath = join(nsPath, 'dist');
+    let canceled = false;
+    cpy('./dist/**', distPath, {flat: false, filter: () => !canceled})
+        .catch(error => {
+            console.warn(`Error occurred while copying`, error);
+        })
+        .then(() => {
+            if (canceled) return;
+            const targetPath = join(nsPath, moduleName);
+            if (existsSync(targetPath)) {
+                deleteSync(targetPath, {force: true});
+            }
+            renameSync(distPath, targetPath);
+            console.log(`Copy finished to project`, projectPath);
         });
+    deploy.signal.addEventListener('abort', () => {
+        canceled = true;
     });
 }
 
-build(async () => {
-    console.log('Watching for file changes started.');
-    watch('./src', {delay: 1000, recursive: true, filter: /\.(json|html|scss|ts)$/}, () => build());
-    watch('./tools', {delay: 1000, recursive: true, filter: /\.(json|js|ts)$/}, () => build());
-    if (!options.modules) return;
-    await copy('./node_modules/**', join(projectPath, 'node_modules'), `node modules to project: ${projectPath}`);
-});
+ngPackagr()
+    .forProject('ng-package.json')
+    .withTsConfig('tsconfig.json')
+    .watch()
+    .subscribe(deployToProject)
